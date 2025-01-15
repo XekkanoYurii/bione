c950c2e127de1c7edd87607c43d4d82b0084905d
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2025-01-15 15:44:53 +0100

    a

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./Contacts.module.css";
+import styles from "./Contacts.module.css";
 
 function Contacts() {
   return (
     <section className="contacts">
       <h1>Contact Us</h1>
       <p>Get in touch with us through the form below.</p>
+      <ul className={styles["ul"]}>
+        <li>lalalal</li>
+        <li>lalala</li>
+      </ul>
     </section>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,25 +3,22 @@ import styles from "./Home.module.css"; // Імпорт стилів як об'�
 
 function Home() {
   return (
-    <section className={styles.home}>
-      {" "}
-      {/* Використання стилів через "styles" */}
-      <h1 className={styles["title-1"]}>Welcome to the Home Page</h1>
-      <p className={styles["text-1"]}>
-        This is the main landing page of the site.
-      </p>
-      <div className="">
-        <h2 className={styles["title-2"]}>Hohohooohoh</h2>
-        <ul className={styles.list["list"]}>
-          <li className={styles["list-tem"]}>
-            <h3 className={styles["title-3"]}>Hello</h3>
-            <p className={styles.text3}>lalalalalaallalalalalalal</p>
-          </li>
-          <li className={styles["list-tem"]}>
-            <h3 className={styles["title-3"]}>Hello</h3>
-            <p className={styles.text3}>lalalalalaallalalalalalal</p>
-          </li>
-        </ul>
+    <section className={styles["hero-section"]}>
+      <div className={styles["hero-container"]}>
+        <h1 className={styles["hero-title"]}>Welcome to the Home Page</h1>
+        <div className={styles[""]}>
+          <h2 className={styles[""]}>Hohohooohoh</h2>
+          <ul className={styles.list[""]}>
+            <li className={styles[""]}>
+              <h3 className={styles[""]}>Hello</h3>
+              <p className={styles[""]}>lalalalalaallalalalalalal</p>
+            </li>
+            <li className={styles[""]}>
+              <h3 className={styles[""]}>Hello</h3>
+              <p className={styles[""]}>lalalalalaallalalalalalal</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
