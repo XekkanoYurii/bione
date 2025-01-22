a734bbd99c31eb082e6db319e117bb77bb636aa1
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2025-01-22 13:41:19 +0100

    add text welcome

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,38 +1,15 @@
-import { useState, useEffect } from "react";
 import styles from "./Welcome.module.css";
 
 const WelcomeSection = () => {
-  const [opacity, setOpacity] = useState(0.5); // Початкове затемнення (50%)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY; // Поточна позиція прокрутки
-      const fadePoint = window.innerHeight * 0.5; // Початок просвітлення
-      const maxPoint = window.innerHeight * 1.3; // Повна видимість
-
-      if (scrollPosition <= fadePoint) {
-        setOpacity(0.5); // Максимальне затемнення
-      } else if (scrollPosition >= maxPoint) {
-        setOpacity(0); // Повністю прозоре
-      } else {
-        const calculatedOpacity =
-          0.5 - ((scrollPosition - fadePoint) / (maxPoint - fadePoint)) * 0.5;
-        setOpacity(calculatedOpacity);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll); // Очищуємо слухач
-    };
-  }, []);
-
   return (
-    <div
-      className={styles["welcome-section"]}
-      style={{ "--overlay-opacity": opacity }} // Передаємо динамічну прозорість
-    ></div>
+    <div className={styles["welcome-section"]}>
+      <div className={styles["text-column-left"]}>
+        Знання поруч, лише простягни руку
+      </div>
+      <div className={styles["text-column-right"]}>
+        Відкрий те, що завжди було перед тобою
+      </div>
+    </div>
   );
 };
 
