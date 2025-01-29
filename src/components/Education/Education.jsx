edb76fb4ededdad8ad74601c4b2965e338ffa435
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2025-01-29 14:57:52 +0100

    a

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,17 @@
 import NavigationButton from "../NavigationButton/NavigationButton";
 
 import BlockList from "./BlockList/BlockList";
+import styles from "./Education.module.css";
 const Education = () => {
   return (
-    <div>
-      Education
+    <div className={styles["education-container"]}>
+      <div className={styles["education-content"]}>
+        <h2 className={styles["education-title"]}>Готові навчатися?</h2>{" "}
+        <p className={styles["education-text"]}>
+          Обирайте тему, що вас цікавить, та занурюйтесь у світ знань разом із
+          B1ONE. Час розвиватися!
+        </p>
+      </div>
       <BlockList />
       <NavigationButton direction="left" to="/about" />
       <NavigationButton direction="right" to="/products" />
