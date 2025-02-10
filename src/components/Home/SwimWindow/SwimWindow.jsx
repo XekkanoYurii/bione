6424b106ee9e13a64c92c8973828466706e063ca
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2025-02-10 20:40:29 +0100

    a

--- src/components/Home/SwimWindow/SwimWindow.jsx
+++ src/components/Home/SwimWindow/SwimWindow.jsx
@@ -2,68 +2,74 @@ import { useState, useEffect } from "react";
 import styles from "./SwimWindow.module.css";
 
 const SwimWindow = () => {
-  const [isVisible, setIsVisible] = useState(false); // Вікно приховане за замовчуванням
+  const [isVisible, setIsVisible] = useState(false);
+  const [closing, setClosing] = useState(false); // Логіка для закриття
 
   useEffect(() => {
-    // Перевіряємо sessionStorage: якщо користувач уже натиснув кнопку, вікно не показується
     const hasClosedPopup = sessionStorage.getItem("hasClosedSwimWindow");
     if (!hasClosedPopup) {
       const handleScroll = () => {
-        const scrollPosition = window.scrollY; // Поточна позиція скролу
-        const triggerPoint = window.innerHeight * 0.55; // Точка появи (1.3 висоти viewport)
+        const scrollPosition = window.scrollY;
+        const triggerPoint = window.innerHeight * 0.5;
 
         if (scrollPosition >= triggerPoint) {
-          setIsVisible(true); // Показуємо вікно
-          window.removeEventListener("scroll", handleScroll); // Знімаємо слухач після спрацювання
+          setIsVisible(true);
+          window.removeEventListener("scroll", handleScroll);
         }
       };
 
       window.addEventListener("scroll", handleScroll);
-
-      return () => {
-        window.removeEventListener("scroll", handleScroll); // Очищаємо слухач при знищенні компонента
-      };
+      return () => window.removeEventListener("scroll", handleScroll);
     }
   }, []);
 
   useEffect(() => {
-    if (isVisible) {
-      document.body.style.overflow = "hidden"; // Заборона скролу
-    } else {
-      document.body.style.overflow = "auto"; // Відновлення скролу
-    }
+    document.body.style.overflow = isVisible ? "hidden" : "auto";
   }, [isVisible]);
 
   const handleClose = () => {
-    setIsVisible(false); // Закриваємо вікно
-    sessionStorage.setItem("hasClosedSwimWindow", "true"); // Зберігаємо стан у sessionStorage
+    setClosing(true); // Запускаємо анімацію закриття
+
+    setTimeout(() => {
+      setIsVisible(false);
+      setClosing(false);
+      sessionStorage.setItem("hasClosedSwimWindow", "true");
+    }); // Час збігається з анімацією
   };
 
-  if (!isVisible) return null; // Не рендеримо, якщо вікно приховано
+  if (!isVisible) return null;
 
   return (
-    <div className={styles["swim-overlay"]}>
-      <div className={styles["swim-window"]}>
+    <div
+      className={`${styles["swim-overlay"]} ${
+        closing ? styles["fade-out"] : ""
+      }`}
+    >
+      <div
+        className={`${styles["swim-window"]} ${
+          closing ? styles["slide-out"] : ""
+        }`}
+      >
         <div className={styles["swim-content"]}>
           <h2 className={styles["title"]}>
-            Welcome to space <span className={styles["span"]}>B1One</span>
+            Раді тебе бачити у <span className={styles["span"]}>B1One</span> тут
+            ти дізнаєшся
           </h2>
           <ul className={styles["list"]}>
             <li className={styles["list-item"]}>
-              - Кастомні AI-моделі для оптимізації процесів
+              Кастомні AI-моделі для оптимізації процесів
             </li>
             <li className={styles["list-item"]}>
-              - Розумна аналітика, яка допомагає приймати кращі рішення
+              Розумна аналітика для кращих рішень
             </li>
             <li className={styles["list-item"]}>
-              - Інноваційні інструменти, що відкривають нові горизонти.
-              Розширюйте можливості
+              Інноваційні інструменти для розширення можливостей
             </li>
             <li className={styles["list-item"]}>
-              - Досліджуйте свіжі підходи до вирішення задач
+              AI-чати для роботи, навчання та автоматизації
             </li>
             <li className={styles["list-item"]}>
-              - Керуйте процесами ефективно з нашими технологіями
+              Персональні AI-асистенти для вашого комфорту
             </li>
           </ul>
           <button className={styles["swim-button"]} onClick={handleClose}>
